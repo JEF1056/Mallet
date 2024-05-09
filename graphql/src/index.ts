316bf8572220e5a414feaa23a3e123e552be194a
Jess Fan <jessefan2002@gmail.com>
2024-05-08 19:29:26 -0700

    add initial foundation for prod deployent

--- graphql/src/index.ts
+++ graphql/src/index.ts
@@ -9,6 +9,7 @@ import cors from "cors";
 import { expressMiddleware } from "@apollo/server/express4";
 import { typeDefs, resolvers } from "./resolvers";
 import { prisma } from "./db";
+import { ApolloServerPluginLandingPageDisabled } from "@apollo/server/plugin/disabled";
 
 export interface GlobalContext {
   // You can optionally create a TS interface to set up types
@@ -32,25 +33,31 @@ const wsServer = new WebSocketServer({
 // Save the returned server's info so we can shutdown this server later
 const serverCleanup = useServer({ schema }, wsServer);
 
+let apolloServerPlugins = [
+  // Proper shutdown for the HTTP server.
+  ApolloServerPluginDrainHttpServer({ httpServer }),
+  // Proper shutdown for the WebSocket server.
+  {
+    async serverWillStart() {
+      return {
+        async drainServer() {
+          await serverCleanup.dispose();
+          await prisma.$disconnect();
+        },
+      };
+    },
+  },
+];
+
+// Disable the Apollo Studio landing page in production.
+if (process.env.NODE_ENV === "production") {
+  apolloServerPlugins.push(ApolloServerPluginLandingPageDisabled());
+}
+
 // Set up ApolloServer.
 const server = new ApolloServer({
   schema,
-  plugins: [
-    // Proper shutdown for the HTTP server.
-    ApolloServerPluginDrainHttpServer({ httpServer }),
-
-    // Proper shutdown for the WebSocket server.
-    {
-      async serverWillStart() {
-        return {
-          async drainServer() {
-            await serverCleanup.dispose();
-            await prisma.$disconnect()
-          },
-        };
-      },
-    },
-  ],
+  plugins: apolloServerPlugins,
 });
 
 await server.start();
@@ -70,4 +77,5 @@ httpServer.listen(process.env.PORT, () => {
   console.log(
     `🔨 Subscription endpoint ready at ws://localhost:${process.env.PORT}/`
   );
+  console.log(`Running in enviornment mode ${process.env.NODE_ENV}`);
 });
